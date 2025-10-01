d280c3dc086b8e6df49de1ccd8f0ff4a86088563
Angelina <vukovic.angelina7@gmail.com>
2025-10-01 17:15:36 +0200

    kreirana kanban aplikacija

---
+++ js/todo.js
@@ -0,0 +1,95 @@
+const dugmeDodajTask=document.getElementById("dodajTask");
+let taskovi= JSON.parse(localStorage.getItem('taskovi')) || [];
+
+
+function sacuvajTaskove() {
+    const taskoviJSON = JSON.stringify(taskovi);
+    localStorage.setItem("taskovi",taskoviJSON);
+}
+
+
+function dodajTask(){
+  const naslovELement=document.getElementById("taskNaslov");
+  const opisElement=document.getElementById("taskOpis");
+  const datumElement=document.getElementById("taskDatum");
+
+  const naslov=naslovELement.value.trim();
+  const opis=opisElement.value.trim();
+  const datum=datumElement.value;
+
+  if(!naslov || !opis || !datum){
+    alert("Sva polja moraju da budu popunjena");
+    return;
+  } 
+
+  let novitask={naslov:naslov,
+                opis: opis,
+                datum: datum,
+                status:"planirano"
+               };
+
+  taskovi.push(novitask);
+  sacuvajTaskove();
+  console.log(taskovi);
+
+
+}
+
+
+function pomeriTask(index){
+   let trenutnitask=taskovi[index];
+   if(trenutnitask.status==="planirano"){
+    trenutnitask.status="utoku";
+   }else if(trenutnitask.status==="utoku"){
+    trenutnitask.status="zavrseno";
+   }else if(trenutnitask.status==="zavrseno"){
+       alert("Ovaj task je vec zavrsen");
+   }
+   razvrstajTaskove();
+}
+
+function obrisiTask(index){
+    if(confirm,"Da li ste sigurni da zelite da obrisete ovaj task?"){
+        taskovi.splice(index,1);
+        sacuvajTaskove();
+        razvrstajTaskove();
+    }
+    
+}
+
+function razvrstajTaskove(){
+  const divPlanirano=document.getElementById("planirano");
+  const divUtoku=document.getElementById("utoku");
+  const divZavrseno=document.getElementById("zavrseno");
+
+divPlanirano.innerHTML= "";
+divUtoku.innerHTML="";
+divZavrseno.innerHTML="";
+
+  taskovi.forEach((task,index) => {
+    const taskElement=document.createElement("div");
+    taskElement.classList.add("task");
+    taskElement.addEventListener("click", () => {pomeriTask(index)});
+    taskElement.innerHTML=`<h3>${task.naslov}</h3>
+    <p>${task.opis}</p>
+    <p>${task.datum}</p>
+    <button class="obrisiBtn">Obrisi</button>`;
+
+    taskElement.querySelector(".obrisiBtn").addEventListener("click",() => {obrisiTask(index)});
+
+    if(task.status==="planirano"){
+        divPlanirano.appendChild(taskElement);
+    }else if(task.status==="utoku"){
+        divUtoku.appendChild(taskElement);
+    }else if(task.status==="zavrseno"){
+        divZavrseno.appendChild(taskElement);
+    }
+  })
+
+}
+
+
+
+dugmeDodajTask.addEventListener("click", () => {dodajTask});
+ console.log(taskovi);
+ razvrstajTaskove();
